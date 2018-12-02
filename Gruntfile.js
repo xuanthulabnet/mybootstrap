043d5052501b6f029c47495dbac49335a8cf7fb7
XTLAB <xtl@xuanthulab.net>
2018-12-02 20:47:12 +0700

    U

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ const sass = require('node-sass');
 module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            src: {
+                files: ['src/customs/*.scss', 'src/mybootstrap.scss'],
+                tasks: ['default'],
+              },
+        },
+
         sass: {
             options: {
                 implementation: sass,
@@ -15,13 +22,39 @@ module.exports = function(grunt) {
                     'build/mybootstrap.css' : 'src/mybootstrap.scss'
                 }
             }
-        }
+        },
+        cssmin: {
+            options: {
+              mergeIntoShorthands: false,
+              roundingPrecision: -1
+            },
+            build: {
+              files: {
+                'build/mybootstrap.min.css' : 'build/mybootstrap.css' 
+              }
+            }
+          },
+          postcss: {
+            options: {                
+                processors: [
+                  require('pixrem')(),
+                  require('autoprefixer')({browsers: 'last 2 versions'}),
+                ]
+              },
+            build: {
+                src: 'build/mybootstrap.css'
+              } 
+          }
+          
     });
 
   //Nạp package
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
 
-
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('runwatch', ['watch']);
+  grunt.registerTask('default', ['sass','postcss', 'cssmin']);
 
 }
